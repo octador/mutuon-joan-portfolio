830fd7ce201009a269699e28a682a65b0a062d41
Mutuon Joan <mutuon.j@gmail.com>
2024-08-21 17:47:47 +0200

    add function count QRcode

--- netlify/funtions/countScan.js
+++ netlify/funtions/countScan.js
@@ -1,18 +1,9 @@
-// netlify/functions/countScan.js
-const fetch = require('node-fetch'); // Utilisé pour faire des requêtes HTTP (si nécessaire)
-
-// Simule un compteur de scan (remplace cela par un stockage persistant comme FaunaDB ou Airtable)
 let scanCount = 0;
 
-exports.handler = async function(event, context) {
-    scanCount += 1; // Incrémentation du compteur
-
-    // Optionnel : Stocker le compteur dans un service externe comme FaunaDB ou Airtable
-
+exports.handler = async function (event, context) {
+    scanCount += 1;
     return {
-        statusCode: 302, // Redirige après l'incrémentation du compteur
-        headers: {
-            'Location': 'https://ton-site-final.com', // Remplace par l'URL de destination
-        },
+        statusCode: 200,
+        body: JSON.stringify({ scanCount }),
     };
 };
